f67fcd301e8ac569e933cf8aa2363c55b37fab31
Anas <17gcs23@meaec.edu.in>
2023-01-04 11:48:20 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-db-URL",
+  projectId: "your-projectId",
+  storageBucket: "your-storageBucket",
+  messagingSenderId: "your-messagingSenderId",
+  appId: "your-appId",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
